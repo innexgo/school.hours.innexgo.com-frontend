359570b74768b6f83a0053fedc3c699c813f6a4c
gpi <gpimpale29@gmail.com>
2020-11-29 12:59:01 -0800

    removed random files

--- settings.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export SERVER_PORT=8080
-export SENDGRID_API_KEY='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'
-export SENDGRID_EMAIL_ADDR='noreply@innexgo.com'
-export SCHOOL_NAME='Squidward Community College'
-export SCHOOL_DOMAIN='sqcc'
